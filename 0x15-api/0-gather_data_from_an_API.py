9b4887c11963807a666f174423f4907a74f83c75
JosePabloBonilla <2750@holbertonschool.com>
2021-09-15 18:30:16 +0000

    [add] file 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,6 +3,9 @@
 using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
+import request
+import sys
+
 
 if __name__ == '__main__':
     userId = sys.argv[1]
