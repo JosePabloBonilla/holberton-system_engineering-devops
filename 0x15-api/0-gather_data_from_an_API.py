306badc95d126ee44c337debd8faeb8ab2748663
JosePabloBonilla <2750@holbertonschool.com>
2021-09-15 18:23:28 +0000

    [add] README.md and file 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+if __name__ == '__main__':
+    userId = sys.argv[1]
+    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
+    url_user = 'https://jsonplaceholder.typicode.com/users/'
+    todo = requests.get(url_todo, params={'userId': userId})
+    user = requests.get(url_user, params={'id': userId})
+
+    todo_dict = todo.json()
+    user_dict = user.json()
+
+    completed_tasks = []
+    total_tasks = len(todo_dict)
+    employee = user_dict[0].get('name')
+
+    for task in todo_dict:
+        if task['completed']:
+            completed_tasks.append(task)
+
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(employee, len(completed_tasks), total_tasks))
+
+            for task in completed_tasks:
+                print("\t {}".format(task.get('title')))
